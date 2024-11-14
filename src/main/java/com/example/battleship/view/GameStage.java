0af57e32f870a38522f106c5b037561cf1f7087b
Kevin Toro <ka.toro30@gmail.com>
2024-11-14 15:00:03 -0600

    Proyecto Batalla Naval

---
+++ src/main/java/com/example/battleship/view/GameStage.java
@@ -0,0 +1,4 @@
+package com.example.battleship.view;
+
+public class GameStage {
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.battleship {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.battleship to javafx.fxml;
+    exports com.example.battleship;
+    exports com.example.battleship.Controller;
+    opens com.example.battleship.Controller to javafx.fxml;
+}
